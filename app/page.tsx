bea381771d9d62d451b9f94296ece9e48199cb3a
Onur Akdeniz <onurakdeniz@outlook.com>
2024-04-10 03:28:39 +0300

    upd link

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import styles from './page.module.css'
 
 export async function generateMetadata(): Promise<Metadata> {
   const frameTags = await getFrameMetadata(
-    `${'https://non-crypto-stuff-ten.vercel.app' || 'http://localhost:3000'}/api`,
+    `${'https://non-crypto-stuff-frame.vercel.app' || 'http://localhost:3000'}/api`,
   )
   return {
     other: frameTags,
